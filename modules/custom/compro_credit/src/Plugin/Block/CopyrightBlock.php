d208ef1b196702234c6b1aab096fff691dfc7385
Garrett Miller <garrett@commercialprogression.com>
2017-03-31 17:12:05 -0500

    copyright block, social block, other things

--- modules/custom/compro_credit/src/Plugin/Block/CopyrightBlock.php
+++ modules/custom/compro_credit/src/Plugin/Block/CopyrightBlock.php
@@ -8,6 +8,8 @@
 namespace Drupal\compro_credit\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 
 /**
  * Provides a block with copyright info.
@@ -22,11 +24,22 @@ class CopyrightBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    //External Uri.
+    $url = Url::fromUri('https://www.umich.edu',
+      array(
+        'attributes' => array(
+          'class' => array(
+            'um-copyright-link',
+          ),
+        ),
+      )
+    );
+    $external_link = Link::fromTextandUrl(t('The Regents of the University of Michigan'), $url);
     // Render array that returns text for block content.
     return array(
       'copyright' => array(
         '#markup' => '&copy; ' . date('Y') . ' ' .
-          t('The Regents of the University of Michigan'),
+          $external_link->toString(),
       ),
     );
   }
