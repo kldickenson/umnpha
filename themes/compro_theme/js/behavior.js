cd89b153dcb3e112386b4bcf5f9d5be043d8d091
Hillary Lewandowski <hillary@commercialprogression.com>
2017-04-05 10:22:44 -0400

    Open external links in new window

--- themes/compro_theme/js/behavior.js
+++ themes/compro_theme/js/behavior.js
@@ -5,6 +5,13 @@
   Drupal.behaviors.comproTheme = {
     attach: function (context, settings) {
 
+      // Give external links target="_blank"
+      var $a = $('a');
+      $a.each(function(i) {
+        if (this.href.length && this.hostname !== window.location.hostname) {
+          $(this).attr('target','_blank');
+        }
+      });
 
       var $menu = $('#block-mainmenu');
       var $search_icon = $('#block-searchicon');
