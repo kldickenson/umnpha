96d2eba2dfd18b56c6e58a7330af0282ea8d4bd0
Garrett Miller <garrett@commercialprogression.com>
2017-04-05 20:57:45 -0400

    this is ugly code but it works :/ staggered animations

--- themes/compro_theme/js/behavior.js
+++ themes/compro_theme/js/behavior.js
@@ -43,34 +43,95 @@
       //Scroll Triggered Slide-Ins
       $(window).scroll(function() {
 
+        //get viewport size, set top of page to variable
         var $scrollTop = $(document).scrollTop();
         var $viewportHeight = $(window).height();
         var $sponsors = $('.section-type-sponsors');
-        var $sponsorPosition = $sponsors.position().top;
+
+
+      ////add animate classes
+        // if ($($sponsors)[0]) {
+        //   $sponsors.find('.field--name-field-sponsors-primary__label,.field--name-field-sponsors-secondary-label,.component-sponsor').addClass('animate-on-view');
+        // }
+        //
+        // //get positions of all elements that will be animated on viewport entry
+        // var $elements = $('.animate-on-view');
+        // var $elPositions = [];
+        //
+        // $.each($elements, function (index, element) {
+        //   var elPosition = $(element).position();
+        //
+        //   $elPositions.push(elPosition);
+        //   console.log(index + ": " + elPosition + "  top: " + elPosition.top)
+        // })
+        //   console.log('elements: ', $elements);
+        //   console.log('elPositions properties:  ' + Object.entries($elPositions));
+
+
+        var $sponsorPrimaryLabelPos = $sponsors.find('.field--name-field-sponsors-primary__label').position().top;
+        var $sponsorSecondaryLabelPos = $sponsors.find('.field--name-field-sponsors-secondary-label').position().top;
+        var $sponsorPrimaryImagePos = $sponsors.find('.field--name-field-sponsors-primary-sponsors').position().top;
+        var $sponsorFirstSecondaryImagePos = $sponsors.find('.field--name-field-sponsors-secondary-sponsor').filter(':first').position().top;
+        var $sponsorSecondSecondaryImagePos = $sponsors.find('.field--name-field-sponsors-secondary-sponsor').filter(':nth-child(2)').position().top;
         var $sponsorHeight = $sponsors.outerHeight();
 
-        //Animate when section-type enters viewport
-        if(($sponsorPosition) <= ($scrollTop + $viewportHeight + 80)) {
-          $sponsors.addClass('animated slideInRight');
+        //Animate when relevant div enters viewport
+        if(($sponsorPrimaryLabelPos) <= ($scrollTop + $viewportHeight + 80)) {
+          $sponsors.find('.field--name-field-sponsors-primary__label').addClass('animated slideInRight');
         };
-      });
 
-      if ($('.section-type-testimonial')[0]){
-        $(window).scroll(function() {
-          var $scrollTop = $(document).scrollTop();
-          var $viewportHeight = $(window).height();
+        if(($sponsorSecondaryLabelPos) <= ($scrollTop + $viewportHeight + 80)) {
+          $sponsors.find('.field--name-field-sponsors-secondary-label').addClass('animated slideInRight');
+        };
 
+        if(($sponsorPrimaryImagePos) <= ($scrollTop + $viewportHeight + 80)) {
+          $sponsors.find('.field--name-field-sponsors-primary-sponsors').addClass('animated slideInRight');
+        };
+
+        if(($sponsorFirstSecondaryImagePos) <= ($scrollTop + $viewportHeight + 80)) {
+          $sponsors.find('.field--name-field-sponsors-secondary-sponsor').filter(':first').addClass('animated slideInRight');
+        };
+
+        if(($sponsorSecondSecondaryImagePos) <= ($scrollTop + $viewportHeight + 80)) {
+          $sponsors.find('.field--name-field-sponsors-secondary-sponsor').filter(':nth-child(2)').addClass('animated slideInRight');
+        };
+
+
+        if ($('.section-type-testimonial')[0]){
           var $testimonial = $('.section-type-testimonial');
           var $testimonialPosition = $testimonial.position().top;
           var $testimonialHeight = $testimonial.outerHeight();
 
           //Animate when section-type enters viewport
           if ($testimonialPosition <= ($scrollTop + $viewportHeight + 80)) {
-            $testimonial.addClass('animated slideInRight');
+            $testimonial.find('.testimonial-image').addClass('animated ' +
+              'slideInRight')
+            $testimonial.find('.field--name-field-testimonial-quote').addClass('animated ' +
+              'slideInRight').delay(100);
+            $testimonial.find('.field--name-field-testimonial-author').addClass('animated ' +
+              'slideInRight').delay(1500);
+            $testimonial.find('.field--name-field-testimonial-credentials').addClass('animated ' +
+              'slideInRight').delay(2000);
           };
-        });
-      };
-
+          // if ($testimonialPosition <= ($scrollTop + $viewportHeight + 80)) {
+          //   $testimonial.find('.testimonial-image').addClass('animated' +
+          //     ' slideInRight').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+          //     function(e) {
+          //
+          //       $testimonial.find('.field--name-field-testimonial-quote').addClass('animated slideInRight').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+          //         function(e) {
+          //
+          //           $testimonial.find('.field--name-field-testimonial-author').addClass('animated slideInRight').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+          //             function(e) {
+          //
+          //               $testimonial.find('.field--name-field-testimonial-credentials').addClass('animated slideInRight');
+          //
+          //             });
+          //         });
+          //     });
+          // };
+        };
+      });
 
 
       //About Page Title
@@ -80,17 +141,14 @@
 
 
       //Sticky Nav
-
       $(window).scroll(function() {
         var position = $(window).scrollTop();
 
-        console.log(position)
         // Header fix.
         if (position > 0) {
           $('.region-header').addClass('header-fixed animated slideInDown');
           $('.layout-content').css('padding-top', '70px');
         }
-
       });
     }
   };
