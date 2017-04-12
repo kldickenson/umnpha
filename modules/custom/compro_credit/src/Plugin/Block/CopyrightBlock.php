0c4c35c2308c610e1939a3365b6b438d15cf9e98
Hillary Lewandowski <hillary@commercialprogression.com>
2017-04-12 13:10:26 -0400

    Adding CC license to Copyright block, uninstalling update module, testimonial styling udpate

--- modules/custom/compro_credit/src/Plugin/Block/CopyrightBlock.php
+++ modules/custom/compro_credit/src/Plugin/Block/CopyrightBlock.php
@@ -42,6 +42,10 @@ class CopyrightBlock extends BlockBase {
       'copyright' => array(
         '#markup' => '&copy; ' . date('Y') . ' ' . $external_link->toString(),
       ),
+      // @see https://creativecommons.org/choose/results-one?license_code=by-nc&amp;jurisdiction=&amp;version=4.0&amp;lang=en
+      'cc_license' => array(
+        '#markup' => '<br><a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/80x15.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.',
+      ),
     );
   }
 
