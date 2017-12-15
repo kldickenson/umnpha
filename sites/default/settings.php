e7e46a5a03f1d35a3ba6e895ce64260565819cea
Hillary Lewandowski <hillary@commercialprogression.com>
2017-12-15 10:06:37 -0500

    Redirect traffic to free https

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -17,7 +17,7 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 include __DIR__ . "/settings.pantheon.php";
 
 /**
- * If there is a local settings file, then include it
+ * If there is a local settings file, then include it.
  */
 $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
@@ -32,7 +32,7 @@ if (defined('PANTHEON_ENVIRONMENT')) {
     $settings['trusted_host_patterns'][] = "{$_ENV['PANTHEON_ENVIRONMENT']}-{$_ENV['PANTHEON_SITE_NAME']}.pantheonsite.io";
     $settings['trusted_host_patterns'][] = "{$_ENV['PANTHEON_ENVIRONMENT']}-{$_ENV['PANTHEON_SITE_NAME']}.panth.io";
 
-    # Replace value with custom domain(s) added in the site Dashboard
+    // Replace value with custom domain(s) added in the site Dashboard.
     $settings['trusted_host_patterns'][] = '^.+\.healthyagingpoll\.org$';
     $settings['trusted_host_patterns'][] = '^healthyagingpoll\.org$';
   }
@@ -41,16 +41,16 @@ else {
   $settings['trusted_host_patterns'][] = '^localhost$';
 }
 
-// Redirect all traffic to non-www. For example healthyagingpoll.org
+// Redirect all traffic to https www.
 if (isset($_SERVER['PANTHEON_ENVIRONMENT']) && ($_SERVER['PANTHEON_ENVIRONMENT'] === 'live') && (php_sapi_name() != "cli")) {
   if ($_SERVER['HTTP_HOST'] == 'live-umnpha.pantheonsite.io' || $_SERVER['HTTP_HOST'] == 'live-umnpha.pantheon.io' || $_SERVER['HTTP_HOST'] == 'live-umnpha.getpantheon.io' || $_SERVER['HTTP_HOST'] == 'live-umnpha.panth.io') {
     header('HTTP/1.0 301 Moved Permanently');
-    header('Location: http://www.healthyagingpoll.org'. $_SERVER['REQUEST_URI']);
+    header('Location: https://www.healthyagingpoll.org' . $_SERVER['REQUEST_URI']);
     exit();
   }
   if ($_SERVER['HTTP_HOST'] == 'healthyagingpoll.org') {
     header('HTTP/1.0 301 Moved Permanently');
-    header('Location: http://www.healthyagingpoll.org'. $_SERVER['REQUEST_URI']);
+    header('Location: https://www.healthyagingpoll.org' . $_SERVER['REQUEST_URI']);
     exit();
   }
 }
