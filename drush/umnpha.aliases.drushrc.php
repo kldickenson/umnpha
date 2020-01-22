56ba71c7fc9737bc09ba81e29dd8f514e8d07c6f
Hillary Lewandowski <hlewan@umich.edu>
2020-01-22 14:47:41 -0500

    Update all drush aliases; add min width to chosen; add style option to text format; path_alias config updates; add field to reports for attaching videos, add section reference field so that sidebar is more customizable, move download button to top of sidebar; CSS updates for sidebar changes

--- drush/umnpha.aliases.drushrc.php
+++ drush/umnpha.aliases.drushrc.php
@@ -1,24 +1,24 @@
 <?php
 
-$aliases['stage'] = array(
-  'uri' => 'stage-umnpha.pantheonsite.io',
-  'db-url' => 'mysql://pantheon:566b5e4051624ce0a62279b6f76ff995@dbserver.stage.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in:10836/pantheon',
+$aliases['develop'] = array(
+  'uri' => 'develop-umnpha.pantheonsite.io',
+  'db-url' => 'mysql://pantheon:2942683933f3489cb2714209916afb66@dbserver.develop.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in:25260/pantheon',
   'db-allows-remote' => TRUE,
-  'remote-host' => 'appserver.stage.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in',
-  'remote-user' => 'stage.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5',
+  'remote-host' => 'appserver.develop.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in',
+  'remote-user' => 'develop.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5',
   'ssh-options' => '-p 2222 -o "AddressFamily inet"',
   'path-aliases' => array(
-    '%files' => 'code/sites/default/files',
+    '%files' => 'files',
     '%drush-script' => 'drush',
   ),
 );
 
-$aliases['build'] = array(
-  'uri' => 'build-umnpha.pantheonsite.io',
-  'db-url' => 'mysql://pantheon:c080d3ac1f3148ea852d11637638e803@dbserver.build.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in:21879/pantheon',
+$aliases['wtwdesign'] = array(
+  'uri' => 'wtwdesign-umnpha.pantheonsite.io',
+  'db-url' => 'mysql://pantheon:357a1c036a8641578684b77887f44e23@dbserver.wtwdesign.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in:16293/pantheon',
   'db-allows-remote' => TRUE,
-  'remote-host' => 'appserver.build.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in',
-  'remote-user' => 'build.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5',
+  'remote-host' => 'appserver.wtwdesign.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in',
+  'remote-user' => 'wtwdesign.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5',
   'ssh-options' => '-p 2222 -o "AddressFamily inet"',
   'path-aliases' => array(
     '%files' => 'files',
@@ -28,26 +28,26 @@ $aliases['build'] = array(
 
 $aliases['dev'] = array(
   'uri' => 'dev-umnpha.pantheonsite.io',
-  'db-url' => 'mysql://pantheon:4e246c8fcdaf4c2994af638c4519f5ba@dbserver.dev.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in:10871/pantheon',
+  'db-url' => 'mysql://pantheon:2b9d615a893f4b708b8f04e23ce5d6c4@dbserver.dev.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in:26725/pantheon',
   'db-allows-remote' => TRUE,
   'remote-host' => 'appserver.dev.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in',
   'remote-user' => 'dev.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5',
   'ssh-options' => '-p 2222 -o "AddressFamily inet"',
   'path-aliases' => array(
-    '%files' => 'code/sites/default/files',
+    '%files' => 'files',
     '%drush-script' => 'drush',
   ),
 );
 
 $aliases['test'] = array(
   'uri' => 'test-umnpha.pantheonsite.io',
-  'db-url' => 'mysql://pantheon:c855bfc62d5646b4a0c21ec03c3dcc27@dbserver.test.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in:10853/pantheon',
+  'db-url' => 'mysql://pantheon:da5dd3cdd95c4dd3ac4575decf6bec01@dbserver.test.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in:13681/pantheon',
   'db-allows-remote' => TRUE,
   'remote-host' => 'appserver.test.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5.drush.in',
   'remote-user' => 'test.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5',
   'ssh-options' => '-p 2222 -o "AddressFamily inet"',
   'path-aliases' => array(
-    '%files' => 'code/sites/default/files',
+    '%files' => 'files',
     '%drush-script' => 'drush',
   ),
 );
@@ -60,7 +60,7 @@ $aliases['prod'] = array(
   'remote-user' => 'live.6d63fa88-8632-4f72-84ec-f0b61ea7b0f5',
   'ssh-options' => '-p 2222 -o "AddressFamily inet"',
   'path-aliases' => array(
-    '%files' => 'code/sites/default/files',
+    '%files' => 'files',
     '%drush-script' => 'drush',
   ),
 );
